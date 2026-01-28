e3bcbf7676f1cc39da431d554b8c7b51a4ff0101
Nethmi Ranathunga <nethmimal121@gmail.com>
2026-01-28 16:37:14 +1030

    linked avatar from profile to chatbox page

--- Interactive AI studio/login/chatbox/chatbox.js
+++ Interactive AI studio/login/chatbox/chatbox.js
@@ -2,8 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const avatarImg = document.getElementById("selectedAvatarImg");
     const savedAvatar = sessionStorage.getItem("selectedAvatar");
 
+    console.log("Avatar from session:", savedAvatar);
+
     if(savedAvatar && avatarImg) {
-        avatarImg.src = savedAavatar;
+        avatarImg.src = savedAvatar;
         avatarImg.style.display = "block";
     }
 });
+
+
